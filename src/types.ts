6b74fbee7785a2ce820e08eac300a3a149af048d
RishikaBothra <rshkbothra@gmail.com>
2025-05-14 00:52:10 +0530

    difference bet types and interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,65 @@
-//-->>interfaces and types
-//-->>simple codes..
+// difference between type and interface
+//interface does not support union types, but type supports union types
+//union can have either or all properties,while intersection must have all properties
 
-// 1.even number checker
 
-function isEven(num:number):boolean{ // here we are giving the return type of the function thats why we are giving boolean
+//1. intersecting types
 
-    if(num%2==0){
-        return true;
-    }
-    else{
-        return false;
-    }
+type Employee = {
+    name:string;
+    startdate :string;
 
 }
 
-console.log(isEven(10));
-console.log(isEven(11));
-
-// 2. greet user
-
-function greetu(name:string):string{
-    return 'hello'+ " " + name;
+type MAnager = {
+    name:string;
+    department:string;
 }
 
-console.log(greetu('riya'));
+type teamlead = Employee & MAnager; // here we are using the intersecting types,here we are using the & operator to combine the two types into one type.
 
+let e:Employee = {
+    name:'fin',
+    startdate:'2023-10-01'
+}
 
+let m:MAnager = {
+    name:'fin',
+    department:'IT'
+}
+
+let tl:teamlead = {
+    name:'fin',
+    startdate:'2023-10-01',
+    department:'IT'
+}// here we are using the intersecting types, so we have to give all the properties of both the types, so we have to give name, startdate and department. If we dont give any one of them then it will give an error.
+
+//2. union types
+
+type Employee1 = {
+    name:string;
+    startdate :string;
+
+}
+
+type MAnager1 = {
+    name:string;
+    department:string;
+}
+
+type teamlead1 = Employee1 | MAnager1; // here we are using the union types,here we are using the | operator to combine the two types into one type.
+
+let e1:Employee1 = {
+    name:'fin',
+    startdate:'2023-10-01'
+}   
+
+let m1:MAnager1 = {
+    name:'fin',
+    department:'IT'
+}
+let tl1:teamlead1 = {
+    name:'fin',
+    startdate:'2023-10-01',
+    department:'IT'
+} // here its not compulsoury to give department and startdate because we are using union types, so we can give any one of them or both of them or none of them.

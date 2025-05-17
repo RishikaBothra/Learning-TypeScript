21352bd413f02a512e8b63e4f413f2bc7f3a1f04
RishikaBothra <rshkbothra@gmail.com>
2025-05-17 22:05:29 +0530

    added the concepts of Exclude  in ts

--- src/exculde.ts
+++ src/exculde.ts
@@ -1,2 +1,12 @@
 // In a function that can accept several types of inputs but you want to exclude specific types from being passed to it.
 
+type Events = 'click' | 'scroll' | 'mousemove';
+type ExcludeEvent = Exclude<Events, 'scroll'>; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent('click'); // OK
+handleEvent('mousemove'); // OK
+// handleEvent('scroll'); // Error: Argument of type '"scroll"' is not assignable to parameter of type 'ExcludeEvent'.
